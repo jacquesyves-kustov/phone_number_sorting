df9d71cdcf046bc72644c7d527bda05450c9c0c6
Sergei Kustov <sergekust@gmail.com>
2023-06-21 02:59:57 +0500

    Refactor: Init resources explicitly

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,28 @@
-from deps.dependencies import file_splitter, file_merger_factory
+from config import AppConfig
+from deps.dependencies import file_merger_factory, file_reader_factory, file_splitter_factory, sort_list
+from services.file_splitter import FileSplitter
+from tools.files.paginated_reader import FilePaginatedReader
+
+
+def _init_resources() -> (FilePaginatedReader, FileSplitter):
+    file_reader = file_reader_factory(
+        directory_path=AppConfig.RESOURCES_PATH,
+        file_name=AppConfig.PHONE_NUMBERS_SOURCE_FILE,
+    )
+
+    file_splitter = file_splitter_factory(
+        output_directory=AppConfig.RESOURCES_PATH,
+        source_file_reader=file_reader,
+        preprocess_handler=sort_list,
+    )
+
+    return file_reader, file_splitter
 
 
 def main():
+    # Step 0) Init resources
+    file_reader, file_splitter = _init_resources()
+
     # Step 1) Split the raw file into several small files
     file_splitter.split_file()
 
