218e6cc084c48e94de30b022a4cee410f46d9252
Sergei Kustov <sergekust@gmail.com>
2023-06-21 04:05:11 +0500

    Increase values in configuration

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ import os
 class AppConfig:
     RESOURCES_PATH = os.path.relpath('..\\resources', os.path.dirname(__file__))
     PHONE_NUMBERS_SOURCE_FILE = 'phones_raw.txt'
-    READER_LINES_PER_FETCH = 4
-    SPLITTER_MAX_ROW_IN_FILE = 3
+    READER_LINES_PER_FETCH = 500
+    SPLITTER_MAX_ROW_IN_FILE = 1_000
     SPLITTER_NEW_FILE_NAME_BASE = 'split_result'
     MERGER_RESULT_FILE_NAME = 'result'
-    ROWS_NUMBER_IN_OUTPUT_FILE = 100
-    LIMIT_ROWS_TO_WRITE_PER_TIME = 30
+    ROWS_NUMBER_IN_OUTPUT_FILE = 10_000
+    LIMIT_ROWS_TO_WRITE_PER_TIME = 500
