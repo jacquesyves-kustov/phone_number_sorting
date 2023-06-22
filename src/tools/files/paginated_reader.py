baba40a942eaae801b7e6b83432c51d3065072f6
sergei <sergekust@gmail.com>
2023-06-22 17:56:23 +0500

    Use bytes offset instead of rows counter while file reading

--- src/tools/files/paginated_reader.py
+++ src/tools/files/paginated_reader.py
@@ -11,18 +11,14 @@ class FilePaginatedReader:
     ):
         self._file_path: str = os.path.join(directory_path, file_name)
         self._rows_per_fetch: Final[int] = rows_per_fetch
-        self._last_fetched_line: int = 0
+        self._prev_byte_offset: int = 0
         self._is_file_read: bool = False
 
     def read_next_lines(self) -> list[str]:
-        current_row = 0
         fetched_lines = []
 
         with open(self._file_path, mode='r') as f:
-            # Move to the last fetched row
-            while current_row != self._last_fetched_line:
-                f.readline()
-                current_row += 1
+            f.seek(self._prev_byte_offset)
 
             # Fetch next '_rows_per_fetch' rows
             for _ in range(self._rows_per_fetch):
@@ -30,8 +26,9 @@ class FilePaginatedReader:
                     self._is_file_read = True
                     break
 
-                self._last_fetched_line += 1
                 fetched_lines.append(line)
+
+            self._prev_byte_offset = f.tell()
 
         return fetched_lines
 
